2da6853778fa4809be9d75b633d9f8423090ab93
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2023-09-13 11:25:26 -0300

    add view more posts function

--- data.js
+++ data.js
@@ -1,25 +1,50 @@
 let dataPosts = [
     {
+        index: 0,
         urlImage: "./assets/imgpost0.png",
         date: "July 23, 2023",
         title: "My new journey as a bootcamp student.",
         paragraph: "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers."
     },{
+        index: 1,
         urlImage: "./assets/imgpost1.png",
         date: "July 23, 2022",
         title: "Blog one",
         paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     },
     {
+        index: 2,
         urlImage: "./assets/imgpost2.png",
         date: "July 233, 2022",
         title: "Blog two",
         paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     },
     {
+        index: 3,
         urlImage: "./assets/imgpost3.png",
         date: "July 2333, 2022",
         title: "Blog three",
+        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+    },
+    {
+        index: 4,
+        urlImage: "./assets/imgpost4.png",
+        date: "September 01, 2023",
+        title: "Blog four",
+        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+    },
+    {
+        index: 5,
+        urlImage: "./assets/imgpost5.png",
+        date: "September 09, 2023",
+        title: "Blog five",
+        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+    },
+    {
+        index: 6,
+        urlImage: "./assets/imgpost6.png",
+        date: "September 13, 2023",
+        title: "Blog six",
         paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     }
 ]
--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import data from './data.js'
 
-const postEl = document.getElementById('post')
 const heroEl = document.getElementById('hero')
+const postEl = document.getElementById('posts')
+const morePostsEl = document.getElementById('morePosts')
+const viewMoreEl = document.getElementById('showMore')
 
 //renderizamos en el front los posts recibidos
 document.addEventListener('DOMContentLoaded', () => {
     //console.log(data);
     let hero = ''
     let posts = ''
-    data.map((post, index) => {
-        if(index === 0){
+    data.map((post) => {
+        if(post.index === 0){
             hero += `
-                <div class="hero" id=${index}>
+                <div class="hero" id=${post.index}>
                     <img src=${post.urlImage} class="hero-img">
                     <div class="hero-description">
                         <p class="hero-date">${post.date}</p>
@@ -20,16 +22,17 @@ document.addEventListener('DOMContentLoaded', () => {
                     </div>
                 </div>
             `
-        } else {
+        } 
+        else if (post.index > 0 && post.index <= 3) {
             posts += `
-                <div class="post-description" id=${index}>
+                <div class="post-description" id=${post.index}>
                     <img class="post-img" src=${post.urlImage}>
                     <p>${post.date}</p>
                     <h1>${post.title}</h1>
                     <p>${post.paragraph}</p>
                 </div>
             `
-        }
+            }
     })
     //console.log(hero);
     //console.log(posts);
@@ -37,6 +40,27 @@ document.addEventListener('DOMContentLoaded', () => {
     postEl.innerHTML = posts
 })
 
+//renderizamos mas posts
+viewMoreEl.addEventListener('click', function(){
+    showMore()
+})
+
+function showMore(){
+    let morePosts = ''
+    data.map((post) => {
+        if(post.index > 3) {
+            morePosts += `
+                <div class="post-description" id=${post.index}>
+                    <img class="post-img" src=${post.urlImage}>
+                    <p>${post.date}</p>
+                    <h1>${post.title}</h1>
+                    <p>${post.paragraph}</p>
+                </div>
+            `
+        }
+        morePostsEl.innerHTML = morePosts
+    })
+}
 function ReadPost(postID) {
     let postToRead = data.find((post, index) => post[index] === postID)
     console.log(postToRead);
