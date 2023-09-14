dba8b9a2d05d6123d9492c4918e8380676948739
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2023-09-14 16:32:48 -0300

    refactor data.js info and html render in js files

--- data.js
+++ data.js
@@ -4,48 +4,147 @@ let dataPosts = [
         urlImage: "./assets/imgpost0.png",
         date: "July 23, 2023",
         title: "My new journey as a bootcamp student.",
-        paragraph: "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers."
-    },{
+        paragraph: "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.",
+        description: {
+            title1: 'How I stay committed to learning',
+            paragraph1: `
+            I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.
+
+            While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.
+            `,
+            title2: 'How I got started',
+            paragraph2: `
+            I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.
+            
+            That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.
+            `,
+        }
+    },
+    {
         index: 1,
         urlImage: "./assets/imgpost1.png",
         date: "July 23, 2022",
         title: "Blog one",
-        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        description: {
+            title1: 'How I stay committed to learning',
+            paragraph1: `
+            I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.
+
+            While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.
+            `,
+            title2: 'How I got started',
+            paragraph2: `
+            I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.
+            
+            That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.
+            `,
+        }
     },
     {
         index: 2,
         urlImage: "./assets/imgpost2.png",
         date: "July 233, 2022",
         title: "Blog two",
-        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        description: {
+            title1: 'How I stay committed to learning',
+            paragraph1: `
+            I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.
+
+            While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.
+            `,
+            title2: 'How I got started',
+            paragraph2: `
+            I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.
+            
+            That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.
+            `,
+        }
     },
     {
         index: 3,
         urlImage: "./assets/imgpost3.png",
         date: "July 2333, 2022",
         title: "Blog three",
-        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        description: {
+            title1: 'How I stay committed to learning',
+            paragraph1: `
+            I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.
+
+            While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.
+            `,
+            title2: 'How I got started',
+            paragraph2: `
+            I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.
+            
+            That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.
+            `,
+        },
     },
     {
         index: 4,
         urlImage: "./assets/imgpost4.png",
         date: "September 01, 2023",
         title: "Blog four",
-        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        description: {
+            title1: 'How I stay committed to learning',
+            paragraph1: `
+            I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.
+
+            While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.
+            `,
+            title2: 'How I got started',
+            paragraph2: `
+            I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.
+            
+            That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.
+            `,
+        }
     },
     {
         index: 5,
         urlImage: "./assets/imgpost5.png",
         date: "September 09, 2023",
         title: "Blog five",
-        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        description: {
+            title1: 'How I stay committed to learning',
+            paragraph1: `
+            I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.
+
+            While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.
+            `,
+            title2: 'How I got started',
+            paragraph2: `
+            I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.
+            
+            That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.
+            `,
+        }
     },
     {
         index: 6,
         urlImage: "./assets/imgpost6.png",
         date: "September 13, 2023",
         title: "Blog six",
-        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+        paragraph: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        description: {
+            title1: 'How I stay committed to learning',
+            paragraph1: `
+            I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.
+
+            While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.
+            `,
+            title2: 'How I got started',
+            paragraph2: `
+            I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.
+            
+            That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.
+            `,
+        }
     }
 ]
 export default dataPosts
--- post.js
+++ post.js
@@ -20,17 +20,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 <h1>${selectedPost.title}</h1>
                 <p>${selectedPost.paragraph}</p>
                 <img class="post-img" src=${selectedPost.urlImage}>
-                <p>
-                    <h4>How I stay committed to learning</h4>
-                    I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.<br>
-                    <br>
-                    While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.<br>
-                    <h4>How I got started</h4>
-                    I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.<br>
-                    <br>
-                    That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.
-                </p>
-            </div>
+                <h4>${selectedPost.description.title1}</4>
+                <p>${selectedPost.description.paragraph1}</p>
+                <h4>${selectedPost.description.title2}</4>
+                <p>${selectedPost.description.paragraph2}</p>
         `
     } else {
         postToRender = `
